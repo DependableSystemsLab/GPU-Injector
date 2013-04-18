8d5fc64454dcf353e74388999745232e1acea375
flyree <bo@alkindi.ece.ubc.ca>
2013-04-18 14:19:49 -0700

    adding analysis tool for SDC

---
+++ analysis.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from sets import Set
+def processCrash(filename):
+    log = open(filename,"r")
+    logs = log.readlines()
+    new_logs = []
+    for log in logs:
+        if "['info" not in log and "activated" not in log and "incorrectly" not in log and "starts" not in log and "New value" not in log:
+            pass
+        else:
+            new_logs.append(log)
+    flag = 0
+    buf = []
+    for log in new_logs:
+        buf.append(log)
+        if "incorrectly" in log:
+                print "#######"
+                print buf
+        if "starts" in log:
+            buf = []
+               
+processCrash("spmv_spmv_jds.log")
+
+            
