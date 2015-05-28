36d8457932c0ae60f2310ad7a636d4b1633f8c75
sudeep-sureshan <saisudeep1994@gmail.com>
2015-05-28 16:16:43 -0700

    Updating_files
    This script will do the following:
    1.>Update the log file(which was obtained as output of profiling) with information required to be processed by 'faultinjection.py"  
    2.>Update configure file 
    3.>Then calls and runs faultInjection.py

--- generateDynamic.py
+++ generateDynamic.py
@@ -1,24 +1,69 @@
-import os
 import sys
-import configure
-import subprocess 
 import pexpect
+import logging
+import time 
+import random
+import configure
+import faultInjection
 
+logger = None
+CUDA_GDB_PATH = "/usr/local/cuda-6.0/bin/cuda-gdb "
 CUDA_GDB_EXPECT = "\(cuda-gdb\)"
-cmd = "cuda-gdb "+configure.binary_path
-argument = configure.parameter
-cuda_gdb_p = pexpect.spawn(cmd)
-cuda_gdb_p.expect(CUDA_GDB_EXPECT)
-cuda_gdb_p.sendline("run "+argument)
-cuda_gdb_p.expect(CUDA_GDB_EXPECT)
-logs = cuda_gdb_p.before
-print logs
+BREAKPOINT="break"
+RUN="run"
+PRINT_PC = "print $pc"
+BREAK_LOCATION = "matrixMul.cu:63" # this is application specific
+NVCC_PATH = "/usr/local/cuda/bin/nvcc "
+THREAD_CONTINUE_EXPECT = "---Type \<return\> to continue, or q \<return\> to quit---"
 
-with open(configure.profile_file,"a") as profile:
-    log = logs.split("\r\n")
-    for line in log:
+
+def main():
+        global CUDA_GDB_PATH,RUN,NVCC_PATH,logger,CUDA_GDB_EXPECT,BREAK_LOCATION,THREAD_CONTINUE_EXPECT
+	#-------------------------------------------------------------------------------------------------------------
+	#if already complied no need these two lines.
+        #child=pexpect.spawn(NVCC_PATH + configure.options +"-g -G " +configure.filename)	#first compile the application with all options required
+	#child.expect(pexpect.EOF,timeout=None)						#expect command prompt				       
+	#-------------------------------------------------------------------------------------------------------------------------
+        child = pexpect.spawn(CUDA_GDB_PATH+" "+configure.binary_path)					 #to enter into cuda-gdb mode
+    	child.maxread = 1000000
+    	child.setecho(False)
+    	child.expect(CUDA_GDB_EXPECT)  
+    	child.sendline(BREAKPOINT+" "+BREAK_LOCATION)
+    	child.expect(CUDA_GDB_EXPECT)
+	wc = child.sendline(RUN)
+    	resend = child.expect([CUDA_GDB_EXPECT,THREAD_CONTINUE_EXPECT])
+    	if resend == 1:
+       		child.sendline()
+    	rawstr = child.before
+	lines = rawstr.split("\r\n")
+	print rawstr  # till here its ok , checked and verified.
+        # Do processimg of rawstr to obtain the required parameters for line "INFO LAUNCH of CUDA Kernel 0 (matrixMulCUDA<32><<<(4,6,1),(32,32,1)>>>) on Device 0"
+        for line in lines:
+	    if "Switching focus" in line:
+		line = line.lstrip().rstrip("\r\n")
+            	temp1 = line.split(" ")
+                items=len(temp1) # 20
+		print items # 20
+		kernel_id= (temp1[5].split(','))[0]  # to obtain 0 from 0,
+		print "Kernel_id: "+str(kernel_id)
+		device_id= (temp1[13].split(','))[0] # to obtain 0 from 0,
+		print "Device_id: "+str(device_id)
+	    elif "Breakpoint" and "<<<" and ">>>" in line:
+		temp1 = line.split(" ")
+		kernel_laucn_config="("+temp1[2]+")" # (matrixMulCUDA<32><<<(4,6,1),(32,32,1)>>>)
+		print kernel_laucn_config
+        # till here its ok , checked and verified.
+        # Write to last line in log file
+        with open(configure.profile_file, "a") as myfile:
+             myfile.write("INFO [LAUNCH of CUDA Kernel "+str(kernel_id)+" "+kernel_laucn_config+ "on Device "+str(device_id)+"]")
+	#find starting pc value
+	child.sendline(PRINT_PC)
+        child.expect(CUDA_GDB_EXPECT)
+        pc_rawstr=child.before
+        lines = pc_rawstr.split("\r\n")  # has two lines	
+	start_pc_value=(lines[1].split(" "))[2] # second line has the $1 = 9049592
+        #writing to config file
+	with open("configure.py", "a") as my_file:
+             my_file.write("startingpc = "+start_pc_value+"\n")
+        faultInjection.fault_main()
         
-        if configure.kernel in line and "Launch" in line:
-            profile.write("INFO "+line+"\n")
-cuda_gdb_p.close()
-  
